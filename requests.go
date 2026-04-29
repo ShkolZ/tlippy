fe2c000798713e7498fe77140bd011c3101f9d7c
ShkolZ <victorbertuch@gmail.com>
2026-04-29 02:07:25 +0200

    getting clips

--- requests.go
+++ requests.go
@@ -16,19 +16,51 @@ type Game struct {
 }
 
 type Games struct {
-	Data []Game
+	Data []Game `json:"data"`
 }
 
-func GetClips(token *Token) {
+type Clip struct {
+	ID          string `json:"id"`
+	CreatorName string `json:"creator_name"`
+	Title       string `json:"title"`
+	Views       int    `json:"view_count"`
+	CreatedAt   string `json:"created_at"`
+}
+type Clips struct {
+	Clips []Clip `json:"data"`
+}
+
+func GetClips(token *Token) error {
 
 	games := getGameId(token)
 
 	query := url.Values{}
+	query.Set("first", "50")
 	query.Set("game_id", games.Data[0].Id)
-	fmt.Println(time.Now())
-	fmt.Println(time.Now().Add(-time.Hour * 24 * 7))
+	query.Set("started_at", time.Now().Add(-time.Hour*24*7).Format(time.RFC3339))
+
+	endpoint := fmt.Sprintf("https://api.twitch.tv/helix/clips?%v", query.Encode())
+	fmt.Println(endpoint)
 	// query.Set("")
-	http.Get("https://api.twitch.tv/helix/clips")
+	req, _ := http.NewRequest("GET", endpoint, nil)
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %v", token.Token))
+	req.Header.Add("Client-Id", os.Getenv("CLIENT_ID"))
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer res.Body.Close()
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	clips := Clips{}
+	if err = json.Unmarshal(data, &clips); err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(clips)
+	return nil
 }
 
 func getGameId(token *Token) *Games {
